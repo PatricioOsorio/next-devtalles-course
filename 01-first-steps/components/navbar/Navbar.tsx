2204464ec7088b65ba06b7987bb6ffd1fd1fd366
Patricio Miguel Osorio Osorio <patricio.osorio@agilityps.mx>
2026-03-06 17:44:59 -0600

    Sección 2: Introducción a Next.js - Next/Link

--- 01-first-steps/components/navbar/Navbar.tsx
+++ 01-first-steps/components/navbar/Navbar.tsx
@@ -1,20 +1,27 @@
+import Link from 'next/link';
+
 export const Navbar = () => {
+  const links = [
+    { name: 'About', href: '/about' },
+    { name: 'Contact', href: '/contact' },
+    { name: 'Pricing', href: '/pricing' },
+  ];
+
   return (
     <div className="navbar bg-primary text-primary-content shadow-sm rounded-lg my-2">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+        <Link href="/" className="btn btn-ghost">
+          Home
+        </Link>
       </div>
+
       <div className="flex-none">
         <ul className="menu menu-horizontal px-1">
-          <li>
-            <a href="/about">About</a>
-          </li>
-          <li>
-            <a href="/contact">Contact</a>
-          </li>
-          <li>
-            <a href="/pricing">Pricing</a>
-          </li>
+          {links.map((link) => (
+            <li key={link.href}>
+              <Link href={link.href}>{link.name}</Link>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
